fc0bae1c9ca574518e65c5019571998e4cdf0a90
James Barnes <james.barnes@sharpcircle.co.uk>
2023-09-29 11:52:27 +0100

    variable port

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Server running at http://localhost:${process.env.PORT}/`);
 });
